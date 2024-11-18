c310c25d4b1a14544ae538d3589ee1884d850251
qazal <qazal.software@gmail.com>
2024-11-18 10:39:57 +0200

    raw diff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::{Args, Parser, Subcommand};
 use colored::*;
-use difference::Changeset;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -21,17 +20,20 @@ enum Cmd {
 }
 
 fn print_diff(v1: &str, v2: &str) {
-    let Changeset { diffs, .. } = Changeset::new(v1, v2, "\n");
-    for diff_cache in &diffs {
-        match diff_cache {
-            difference::Difference::Same(part) => println!("{}", part),
-            difference::Difference::Add(part) => part
-                .split("\n")
-                .for_each(|l| println!("{}", format!("+ {}", l).green())),
-            difference::Difference::Rem(part) => part
-                .split("\n")
-                .for_each(|l| println!("{}", format!("- {}", l).red())),
+    let diffable = usize::min(v1.len(), v2.len());
+    let good_lines = Vec::from_iter(v1.lines());
+    for (i, compare) in v2.lines().enumerate() {
+        let good = good_lines[i];
+        match compare == good {
+            true => println!("{good}"),
+            false => {
+                println!("{}", format!("- {}", good).red());
+                println!("{}", format!("+ {}", compare).green());
+            }
         }
+    }
+    if diffable < usize::max(v1.len(), v2.len()) {
+        println!("{}", format!("diffed less lines").yellow())
     }
 }
 
