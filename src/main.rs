eafcd6ac114ca2269d1574d4945a090aa462d82d
qazal <qazal.software@gmail.com>
2024-11-23 17:42:43 +0200

    need this for BUFFER stuff

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ struct DifferArgs {
 
 #[derive(Args, Debug)]
 struct DiffArgs {
-    // Files to compare
-    files: Vec<String>,
+    /// files to compare
+    #[clap(num_args(2))]
+    file: Vec<String>,
+    /// only diff lines that contain this
+    #[clap(last = true)]
+    pattern: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
@@ -18,40 +22,38 @@ enum Cmd {
     Diff(DiffArgs),
 }
 
-fn print_diff(v1: &str, v2: &str) {
-    let diffable = usize::min(v1.len(), v2.len());
-    let good_lines = Vec::from_iter(v1.lines());
-    for (i, compare) in v2.lines().enumerate() {
-        let good = good_lines[i];
-        match compare == good {
-            true => println!("{good}"),
-            false => {
-                println!("\x1b[31m- {}\x1b[0m", good);
-                println!("\x1b[32m+ {}\x1b[0m", compare);
-            }
-        }
-    }
-    if diffable < usize::max(v1.len(), v2.len()) {
-        println!("\x1b[33mdiffed less lines\x1b[0m");
-    }
-}
-
-/// Usage: diff f1 f2
-fn diff_files(files: Vec<String>) {
-    if files.len() != 2 {
-        panic!("Only two files are supported");
-    }
-    let file_contents = files
-        .iter()
-        .map(|file| std::fs::read_to_string(file).unwrap())
-        .collect::<Vec<String>>();
-    print_diff(&file_contents[0], &file_contents[1]);
-}
-
 fn main() {
     let args = DifferArgs::parse();
     match args.cmd {
-        Some(Cmd::Diff(args)) => diff_files(args.files),
+        Some(Cmd::Diff(args)) => {
+            let file_contents = args
+                .file
+                .iter()
+                .map(|file| std::fs::read_to_string(file).unwrap())
+                .collect::<Vec<String>>();
+            let (v1, v2) = (&file_contents[0], &file_contents[1]);
+            let diffable = usize::min(v1.len(), v2.len());
+            let good_lines = Vec::from_iter(v1.lines());
+            for (i, compare) in v2.lines().enumerate() {
+                let good = good_lines[i];
+                let diff = match &args.pattern {
+                    Some(pat) => good.contains(pat) && compare != good,
+                    None => compare != good,
+                };
+                match diff {
+                    false => {
+                        println!("{good}");
+                    }
+                    true => {
+                        println!("\x1b[31m- {}\x1b[0m", good);
+                        println!("\x1b[32m+ {}\x1b[0m", compare);
+                    }
+                }
+            }
+            if diffable < usize::max(v1.len(), v2.len()) {
+                println!("\x1b[33mdiffed less lines\x1b[0m");
+            }
+        }
         None => println!("No subcommand was used"),
     }
 }
